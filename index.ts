afc5cbd8b0ade3deb8ae722b9f1f6713dcf721fa
Xavier Brinon <xavierbrinon@Xaviers-MacBook-Pro.local>
2025-02-05 06:48:19 +0000

    feat: 🌟 interact via the cli

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
+import * as readline from 'node:readline/promises'
+import { stdin as input, stdout as output } from 'node:process'
+import assert from 'node:assert/strict'
 import ollama from 'ollama'
 import type { ChatRequest, Message } from 'ollama'
 
+const cli = readline.createInterface({ input, output })
+
+const cliResponse = await cli.question('What do you want to aks the agent?')
+assert.ok(cliResponse !== '', 'cliResponse: cli.question returned empty string.')
+cli.close()
+
 const message: Message = {
   role: 'user',
-  content: 'Why is the sky blue?'
+  content: cliResponse
 }
 
 const models: Record<string, ChatRequest['model']> = {
@@ -13,7 +22,7 @@ const models: Record<string, ChatRequest['model']> = {
 }
 
 const response = await ollama.chat({
-  model: models.deepseek,
+  model: models.mistral,
   messages: [message],
   stream: true
 })
